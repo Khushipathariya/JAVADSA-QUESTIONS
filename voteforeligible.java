b6c5a5b08b21c353862b8f8be8a61851eaf138e7
khushi pathariya <khushipathariya337@gmail.com>
2025-11-05 00:15:38 +0530

    Add files via upload

---
+++ voteforeligible.java
@@ -0,0 +1,12 @@
+public class voteforeligible {
+    public static void main(String[] args){
+        int age = 16;
+        if(age>=18){
+            System.out.println("she/him is eligible to vote");
+        }
+        else{
+            System.out.println("she/him is not eligible to vote");
+        }
+    }
+    
+}
